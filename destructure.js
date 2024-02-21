740992a1d5facacce9b5a8a5fb1367aa5f83590e
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-21 23:50:12 +0600

    destructure

---
+++ destructure.js
@@ -0,0 +1,10 @@
+let myObj = {
+    name : "Nayeem Khan",
+    age : 25,
+    phone : "01619981998",
+    salary : 10000
+}
+
+let {age: boyosh} = myObj // destructure age for my object
+
+console.log(boyosh);
