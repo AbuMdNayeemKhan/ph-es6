6d5d6ddf05a5a217c032eeae0952ea64010f00a2
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-21 21:15:00 +0600

    es6 template

---
+++ template.js
@@ -0,0 +1,9 @@
+const first = 'Nayeem';
+const last = 'Khan';
+const name = first + ' ' + last;
+console.log(name);
+
+const a = 10;
+const b = 10;
+const result = `a value is: ${a} and b value is: ${b}`;
+console.log(result);
