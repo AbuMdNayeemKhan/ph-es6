a0fd7f325fd9fd892e13f931680f887db16d2724
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-21 23:57:54 +0600

    destructure array

--- destructure.js
+++ destructure.js
@@ -5,6 +5,11 @@ let myObj = {
     salary : 10000
 }
 
+let myarr = [11, 22, 44, 33, 66, 55, 77, 88, 99,];
+let [a, b] = myarr
+
 let {age: boyosh} = myObj // destructure age for my object
 
 console.log(boyosh);
+
+console.log(b);
