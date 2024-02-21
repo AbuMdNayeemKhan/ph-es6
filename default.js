3117d0ac34c646d4a995c6e1ae05e7b7b3eddc5a
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-21 21:01:41 +0600

    es6 default value

---
+++ default.js
@@ -0,0 +1,6 @@
+function add(num1 = 0, num2 = 0) { // use default valu for if default any value is not will set
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+const sum = add(7);   
