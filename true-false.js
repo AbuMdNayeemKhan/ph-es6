a522954c877a4924baf64b79292ee0a0a2752130
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-25 23:57:31 +0600

    true-false and undefine

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//Truthy:
+//1. true
+//2. any number (+ve, -ve) will be truthy other than 0
+//3. any string other than emty string 
+//4. empty object
+//5. empty array
+
+
+//False:
+//1. false
+//2. 0
+//3. '' (empty string)
+//4. undefined
+//5. null
+
+const x = false;
+if(x){
+    console.log('value of x is truthy');
+} else {
+    console.log('value of x is falsy');
+}
---
+++ undefine.js
@@ -0,0 +1,8 @@
+// 1. variable that is not initialized will give nudefined
+// 2. function with no return
+// 3. parameter that is not passed will be dudefined
+// 4. if return has nothing on the right side will return undefined
+// 5. property that doesn't exists on an object will give you undefined
+// 6. accessing array elements outside of the index range
+// 7. deleting an element inside an array
+// 8. set a value derectly to undefined
